1c74b77d553bfa84afab6fe27cf8eb17a873ce7a
jaehyeok <leejaehyeok@INCHEON-PC.localdomain>
2025-07-09 20:19:21 +0900

    테스트 커밋

--- payload/_global.ts
+++ payload/_global.ts
@@ -3,7 +3,7 @@ import previewImage from '../asset/preview.jpg';
 import { IGlobal } from '../component/common/IGlobal';
 
 const title = '이재혁 이력서';
-const description = '백엔드 개발자 지원';
+const description = '자동화 엔지니어 (PLC Enginner)d';
 
 export const _global: IGlobal.Payload = {
   favicon,
